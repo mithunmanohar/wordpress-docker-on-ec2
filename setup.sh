7e72ce172010cec992ebd8bd7071ec28c3e03534
root <root@ip-172-31-36-25.ap-south-1.compute.internal>
2020-07-05 20:25:18 +0000

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo service docker start
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose up -d
+
